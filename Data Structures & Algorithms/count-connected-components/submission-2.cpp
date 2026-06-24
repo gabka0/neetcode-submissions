98b1c3df47b529b353a2123379bd1d91f899ad60
gabka0 <153498433+gabka0@users.noreply.github.com>
2026-06-24 15:22:22 +0800

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        int count=0;
+        unordered_set<int> seen;
+        vector<vector<int>> adj(n);
+        for(int i=0; i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+        for(int i=0; i<n; i++){
+            if(!seen.contains(i)) {
+                count++;
+                dfs(seen, adj, i);
+            }
+        }
+        return count;
+
+    }
+    void dfs(unordered_set<int>& seen, vector<vector<int>>& adj, int i){
+        if(adj[i].size()==0 || seen.contains(i)) return;
+        seen.insert(i);
+        for(int j=0; j<adj[i].size();j++){
+            dfs(seen, adj, adj[i][j]);
+        }
+    }
+};
